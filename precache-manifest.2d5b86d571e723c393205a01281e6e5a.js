e3435725c54a22acac42dadb0e52f2f8706447e0
vishnumd91 <vishnudivakaran1@gmail.com>
2020-08-01 16:10:47 +0530

    Updates

---
+++ precache-manifest.2d5b86d571e723c393205a01281e6e5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26cefe151dbeb88eb953304d2c4aa32a",
+    "url": "/First-React-App/index.html"
+  },
+  {
+    "revision": "dca5f8241ae1c583a659",
+    "url": "/First-React-App/static/css/main.7ee22140.chunk.css"
+  },
+  {
+    "revision": "2fa886befb43f26993c8",
+    "url": "/First-React-App/static/js/2.4171fd46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/First-React-App/static/js/2.4171fd46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca5f8241ae1c583a659",
+    "url": "/First-React-App/static/js/main.7695394d.chunk.js"
+  },
+  {
+    "revision": "a17bac5dcba321738925",
+    "url": "/First-React-App/static/js/runtime-main.d2167d4b.js"
+  }
+]);
